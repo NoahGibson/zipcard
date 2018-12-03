6e3d3b4f4294c3b8fbf0a0729c12eb995d0b29de
Noah Gibson <135noahg@gmail.com>
2018-12-02 21:04:48 -0500

    basic linkedin login functionality

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,22 +1,9 @@
 import { Component } from '@angular/core';
 
-import {LinkedIn, LinkedInLoginScopes} from '@ionic-native/linkedin/ngx';
-
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  scopes: LinkedInLoginScopes[] = ['r_basicprofile', 'r_emailaddress', 'rw_company_admin', 'w_share'];
-
-  constructor(private linkedin: LinkedIn) {}
-
-  login() {
-      this.linkedin.login(this.scopes, true)
-          .then(() => console.log('Logged in!'))
-          .catch(e => console.log('Error logging in', e));
-  }
-
 }
---
+++ src/app/pages/login/login.page.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+
+import {LinkedIn, LinkedInLoginScopes} from '@ionic-native/linkedin/ngx';
+import {Router} from '@angular/router';
+
+@Component({
+    selector: 'app-login',
+    templateUrl: 'login.page.html',
+    styleUrls: ['login.page.scss'],
+})
+export class LoginPage {
+
+    isLoggedIn  = false;
+
+    scopes: LinkedInLoginScopes[] = ['r_basicprofile'];
+
+    constructor(private router: Router, private linkedin: LinkedIn) {}
+
+    ionViewDidAppear() {
+        this.linkedin.hasActiveSession().then((active) => {
+            this.isLoggedIn = active;
+        });
+    }
+
+    login() {
+        this.linkedin.login(this.scopes, true)
+            .then(() =>  this.isLoggedIn = true)
+            .catch(e => console.log('Error logging in', e));
+    }
+
+    logout() {
+        this.linkedin.logout();
+        this.isLoggedIn = false;
+    }
+
+}
