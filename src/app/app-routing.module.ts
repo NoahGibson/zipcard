3a54fa216eb6e77af3ff0aeaa4e14ec1bb8426ef
Noah Gibson <135noahg@gmail.com>
2018-12-03 23:33:26 -0500

    added auth guard to home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AuthGuard} from '@app/core';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', loadChildren: '@app/pages/login/login.module#LoginPageModule' },
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: 'login', loadChildren: '@app/pages/login/login.module#LoginPageModule' },
+    { path: 'home', loadChildren: '@app/pages/home/home.module#HomePageModule', canActivate: [AuthGuard] }
 ];
 
 @NgModule({
